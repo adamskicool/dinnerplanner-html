20cebe3cd40703a4f2d8f3d17f3677d329047337
adamjoha <adamtorkkeli@gmail.com>
2019-02-06 15:18:38 +0100

    fixat en bugg där man inte kunde se print out recipe screen om inte alla recept hade descriptions

--- js/view/dishPrintOutView.js
+++ js/view/dishPrintOutView.js
@@ -17,11 +17,15 @@ class dishPrintOutView {
                 var preparation = "";
 								// Build a string for all types of current dish
                 for(var j = 0; j < this.fullMenu[i].dishTypes.length; j++){
-                    types += this.fullMenu[i].dishTypes[j];
+                    types += this.fullMenu[i].dishTypes[j] + " ";
                 }
 								//Build a string for all instruction steps where every step wraps in a <p></p> element.
-                for(var j = 0; j < this.fullMenu[i].analyzedInstructions[0].steps.length; j++){
-                    preparation += "<p>" + this.fullMenu[i].analyzedInstructions[0].steps[j].number + ". " + this.fullMenu[i].analyzedInstructions[0].steps[j].step + "</p>";
+                if(this.fullMenu[i].analyzedInstructions.length > 0) {
+                    for(var j = 0; j < this.fullMenu[i].analyzedInstructions[0].steps.length; j++){
+                        preparation += "<p>" + this.fullMenu[i].analyzedInstructions[0].steps[j].number + ". " + this.fullMenu[i].analyzedInstructions[0].steps[j].step + "</p>";
+                    }
+                } else {
+                    preparation = "No preparation available.";
                 }
 
         // Add types and preparation string together with the dish image to the print out part of html       
